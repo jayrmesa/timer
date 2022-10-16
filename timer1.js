69b3b362c6fac57b0e30ef9c76a396efed5974cb
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-15 19:24:49 -0600

    Simple Timer

---
+++ timer1.js
@@ -0,0 +1,13 @@
+const beeper = (seconds) => {
+
+  if (isNaN(seconds) || seconds < 0) return;
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, seconds * 1000);
+};
+
+const alarms = process.argv.slice(2);
+
+alarms.forEach(alarm => beeper(alarm));
+
